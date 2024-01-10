206e1102c40147785c65797f835024119a79094b
humblots <shumblot25@gmail.com>
2024-01-10 09:34:49 +0100

    Initial Commit

---
+++ Loot/LootApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootApp.swift
+//  Loot
+//
+//  Created by HUMBLOT Stéphane on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
